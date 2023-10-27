2842e2ede9d471d5f5bf06fbe6825d830afbe85b
Kendall Martinez <kendallmartinezcarvajal4@gmail.com>
2023-10-27 16:34:23 -0600

    Comentario parciales
    faltan kron y tridiag

--- pregunta5_E2.m
+++ pregunta5_E2.m
@@ -1,4 +1,12 @@
-
+%Funcion pregunta5_E2() implementa la ecuacion compleja de Helmholtz para obtener matrices de diferentes tamaños.
+% que nos permiten obtener diferentes soluciones del problema de sistemas de ecuaciones con valores complejos.
+% ai como ver la eficiencia de cada uno de los metodos implementados en las demas preguntas.
+% Sintaxis de la funcion: pregunta5_E2().
+%
+% Parametros de entrada:
+%
+% Parametros de salida:
+%
 function pregunta5_E2()
   clc; clear;
 
@@ -9,7 +17,7 @@ function pregunta5_E2()
     m = mvalues(i);
     h = 1 / (m + 1);
     [W,T] = calc_W_T(m,h);
-    [f,g] = calc_f_g(m,h);
+    [f,g] = calc_f_g(m);
 
     disp(["Caso", num2str(i), ": m=", num2str(m)]);
     pregunta1(W, T, f, g);
@@ -17,6 +25,18 @@ function pregunta5_E2()
 
 end
 
+%Funcion [W,T]=calc_W_T(m,h) implementa un algoritmo que nos permite crear los valores para las matrices de tamano m*m.
+%
+% Sintaxis de la funcion: [W,T]=calc_W_T(m,h)
+%
+% Parametros de entrada:
+%             m = el tamano de la matriz que se desea crear.
+%             h = es el tamano de la malla que se desea crear.
+%
+% Parametros de salida:
+%             W = la primera matriz del sistema que sumada a T equivalen a A.
+%             T = la segunda matriz del sistema que multiplicada por i y sumada con W equivale a A.
+%
 function [W,T]=calc_W_T(m,h)
   A = tridiag(-1,2,-1,m);
   Vm = (h^-2)*A;
@@ -29,13 +49,25 @@ function [W,T]=calc_W_T(m,h)
   T = cita2*I;
 end
 
-function [f,g]=calc_f_g(m,h)
+%Funcion [f,g]=calc_f_g(m) implementa un algoritmo que nos permite crear los valores para las matrices de tamano m*m.
+%
+% Sintaxis de la funcion: [f,g]=calc_f_g(m)
+%
+% Parametros de entrada:
+%             m = el tamano de la matriz que se desea crear.
+%
+% Parametros de salida:
+%             f = la primera matriz del sistema que sumada a g equivalen a b.
+%             g = la segunda matriz del sistema que multiplicada por i y sumada con f equivale a b.
+%
+function [f,g]=calc_f_g(m)
   A = tridiag(-1,2,-1,m);
   Im = eye(m,1);
   f = A*Im;
   g = A*Im;
-
   end
+
+
 
 function KronAB=kron(A,B)
   [m, n] = size(A);
@@ -47,6 +79,8 @@ function KronAB=kron(A,B)
     end
   end
 end
+
+
 
 function A = tridiag(a, b, c, n)
   A = b * eye(n);
